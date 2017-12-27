416e84141d8c6c4e36833564be7db39abf427313
chisl <info@chisl.io>
2017-12-27 18:12:12 +0100

    Initial commit

---
+++ DW1000_constants.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""DW1000: A fully integrated single chip Ultra Wideband (UWB) low-power low-cost transceiver IC for 2-way ranging or TDOA location systems."""
+
+__author__     = "ChISL"
+__copyright__  = "TBD"
+__credits__    = ["Decawave"]
+__license__    = "TBD"
+__version__    = "0.1"
+__maintainer__ = "https://chisl.io"
+__email__      = "info@chisl.io"
+__status__     = "Test"
+
+class REG:
+	DEV_ID = 0
+	SYS_CFG = 4
+	TX_FCTRL = 8
